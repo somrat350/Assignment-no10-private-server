5a8e2b6c0a7c0c1b947003adb2aa1873866fa5fa
somrat350 <mdsomratsordaro350@gmail.com>
2025-11-11 15:09:29 +0600

    successfully connected to the mongodb atlas and created client

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 3000;
 const app = express();
 
@@ -7,9 +9,28 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/",(req,res)=>{
-  res.send("Hello")
-})
+const uri = `mongodb+srv://${process.env.DB_ADMIN}:${process.env.DB_PASSWORD}@cluster0.lechzdi.mongodb.net/?appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Server running in http://localhost:${port}`);
